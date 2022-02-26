32ec65b563c21a90e14171fb4924dfc5c6a6bad8
Meghnanaidu <78645602+Meghnanaidu@users.noreply.github.com>
2022-02-26 19:58:06 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,23 @@
+Status = "";
+Bed_image = "";
+
+function preload() {
+  img = loadImage("bedroom.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(640, 350);
+    canvas.position(315, 200);
+}
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+  fill("rgb(92, 7, 49)");
+  text("Bed", 45, 75);
+  noFill();
+  stroke("rgb(92, 7, 49)");
+  rect(30, 60, 450, 350);
+}
+
+
+
